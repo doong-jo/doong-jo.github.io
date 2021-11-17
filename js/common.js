f505e8ac4553cedcaeb51acaca29da13e2fc15c3
Sungdong Jo <sdong001@gmail.com>
2021-11-17 16:09:37 +0900

    Modify about

--- js/common.js
+++ js/common.js
@@ -1,1 +1,9 @@
 console.log('Hello stranger.');
+
+document.querySelector('h1').onclick = (ev) => {
+  navigator.share({
+    title: document.title,
+    text: 'Hello World',
+    url: 'https://developer.mozilla.org',
+  }); // share the URL of MDN
+};
