b5c79486af2eb51d31ca1f746ad0cba0f2841012
doong-jo <sdong001@gmail.com>
2023-11-28 19:19:11 +0900

    테크리드 1년 회고 추가

--- nav/nav.js
+++ nav/nav.js
@@ -1,33 +1,3 @@
-const navData = [
-  {
-    href: "/index.html",
-    textContent: "Post",
-  },
-  {
-    href: "/about.html",
-    textContent: "About",
-  },
-];
-
-function createNav() {
-  const navElement = document.querySelector("nav");
-  const ulElement = document.querySelector("nav > #nav-list");
-
-  navElement.appendChild(ulElement);
-
-  navData.forEach(({ href, textContent }) => {
-    const navItem = document.createElement("li");
-
-    const linkElement = document.createElement("a");
-    linkElement.href = href;
-    linkElement.textContent = textContent;
-    linkElement.id = "nav-" + textContent.toLowerCase();
-
-    navItem.appendChild(linkElement);
-    ulElement.appendChild(navItem);
-  });
-}
-
 function highlightCurrentNavItem() {
   const navItems = document.querySelectorAll("nav ul li a");
   const currentPath = window.location.pathname;
@@ -45,5 +15,4 @@ function highlightCurrentNavItem() {
   }
 }
 
-createNav();
 highlightCurrentNavItem();
